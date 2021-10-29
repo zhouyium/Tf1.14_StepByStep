9a7139f77f7c21f37ae767477d6a88fae5b57289
zhouyi <70430188+zhouyium@users.noreply.github.com>
2021-10-29 11:59:36 +0800

    第二次课程通过200次学习的方式来逼近y=x*0.1+0.3

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,5 @@
 '''
-本次课程将通过学习的方式来逼近y=x*0.1+0.3
+本次课程将通过200次学习的方式来逼近y=x*0.1+0.3
 '''
 import tensorflow.compat.v1 as tf
 import numpy as np
@@ -8,7 +8,7 @@ import numpy as np
 x_data=np.random.rand(100).astype(np.float32)
 y_data=x_data*0.1 + 0.3
 
-# create tensorflow structure
+# create TensorFlow structure
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases  = tf.Variable(tf.zeros([1]))
 y = Weights * x_data + biases
@@ -18,11 +18,12 @@ loss = tf.reduce_mean(tf.square(y-y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
-# begin tf sesson
+# define TensorFlow session
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+# training
 for step in range(201):
     sess.run(train)
     if 0==(step % 20):
